a106c645a699527742e4e7e82124c6fa2e8f9f17
hilal kawam <hilalkawam92@gmail.com>
2018-12-20 15:27:02 +0200

    bonus

---
+++ Turbulence/turbulence2d.c
@@ -0,0 +1,19 @@
+#include "grid/multigrid.h"
+#include "navier-stokes/stream.h"
+
+//The domain is square of size unity by default. The resolution is constant at 256^2
+int main() {
+  init_grid (256);
+  run();
+}
+
+//The initial condition for vorticity is just a white noise in the range [−1:1]
+event init (i = 0) {
+  foreach()
+    omega[] = noise();
+}
+//We generate images of the vorticity field every 8 timesteps up to t=1000. We fix the colorscale to [−0.3:0.3]
+event output (i += 8; t <= 1000) {
+  static FILE * fp = fopen ("omega.ppm256", "w");
+  output_ppm (omega, fp, min = -0.3, max = 0.3);
+}
